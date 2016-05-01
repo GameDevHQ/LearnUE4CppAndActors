9c1777c643553876362eac03ebc85e9358014058
Valeryi Savich <Relrin78@gmail.com>
2016-05-01 12:36:36 +0300

    Added MeleeWeapon class for a monsters

---
+++ Source/LearnUE4CppAndActors/MeleeWeapon.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LearnUE4CppAndActors.h"
+#include "MeleeWeapon.h"
+
+
+AMeleeWeapon::AMeleeWeapon(const class FObjectInitializer& PCIP): Super(PCIP)
+{
+    AttackDamage = 1;
+    Swinging = false;
+    WeaponHolder = nullptr;
+    
+    Mesh = PCIP.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("Mesh"));
+    RootComponent = Mesh;
+    
+    ProxBox = PCIP.CreateDefaultSubobject<UBoxComponent>(this, TEXT("ProxBox"));
+    ProxBox->OnComponentBeginOverlap.AddDynamic(this, &AMeleeWeapon::Prox);
+    ProxBox->AttachTo(RootComponent);
+}
+
+
+void AMeleeWeapon::Prox_Implementation(AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                                       int32 OtherBodyIndex, bool bFromSweep,
+                                       const FHitResult & SweepResult)
+{
+    if (OtherComp != OtherActor->GetRootComponent()) return;
+    
+    // Avoid hitting things while sword isn't swigging,
+    // Avoid hitting youself, and
+    // Avoid hitting the same OtherActor twice
+    if (Swinging && OtherActor != WeaponHolder && !ThingsHit.Contains(OtherActor))
+    {
+        OtherActor->TakeDamage(AttackDamage + WeaponHolder->BaseAttackDamage, FDamageEvent(),
+                               NULL, this);
+        ThingsHit.Add(OtherActor);
+    }
+}
+
+
+void AMeleeWeapon::Swing()
+{
+    ThingsHit.Empty();
+    Swinging = true;
+}
+
+
+void AMeleeWeapon::Rest()
+{
+    ThingsHit.Empty();
+    Swinging = false;
+}
---
+++ Source/LearnUE4CppAndActors/MeleeWeapon.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Monster.h"
+#include "MeleeWeapon.generated.h"
+
+
+UCLASS()
+class LEARNUE4CPPANDACTORS_API AMeleeWeapon : public AActor
+{
+    GENERATED_UCLASS_BODY()
+
+    // The amount of damage attacks do
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MeleeWeapon)
+    float AttackDamage;
+    
+    // A list of things the melee weapon already hit this swing?
+    // Ensures each thing sword passes thru only gets hit once.
+    TArray<AActor*> ThingsHit;
+    
+    // Prevents damage from occurring on frames where the sword is not swinging.
+    bool Swinging;
+    
+    // "Stop hitting yourself" - used to check if the actor holding
+    // the weapon is hitting himself
+    AMonster* WeaponHolder;
+    
+    // The sphere you collide with to pick item up
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = MeleeWeapon)
+    UBoxComponent* ProxBox;
+    
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = MeleeWeapon)
+    UStaticMeshComponent* Mesh;
+    
+    UFUNCTION(BlueprintNativeEvent, Category = Collision)
+    void Prox( AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult );
+    
+    void Swing();
+    void Rest();
+};
